5952bd8abb5f80610181f231159405cb53474569
Nora Marcelli <noramarcelli@gmail.com>
2018-05-03 15:16:26 +0300

    commit

--- src/views/DogPreviewPage.vue
+++ src/views/DogPreviewPage.vue
@@ -16,37 +16,8 @@
         <i class="fa fa-remove"></i>
         <i class="fa fa-heart"></i>
       </div>  
-<<<<<<< HEAD
-     -->
-    <!-- <div class="tinder--card">
-      <img src="https://placeimg.com/600/300/people">
-      <h3>Demo card 1</h3>
-      <p>This is a demo for Tinder like swipe cards</p>
-    </div>
-    <div class="tinder--card">
-      <img src="https://placeimg.com/600/300/animals">
-      <h3>Demo card 2</h3>
-      <p>This is a demo for Tinder like swipe cards</p>
-    </div>
-    <div class="tinder--card">
-      <img src="https://placeimg.com/600/300/nature">
-      <h3>Demo card 3</h3>
-      <p>This is a demo for Tinder like swipe cards</p>
-    </div>
-    <div class="tinder--card">
-      <img src="https://placeimg.com/600/300/tech">
-      <h3>Demo card 4</h3>
-      <p>This is a demo for Tinder like swipe cards</p>
-    </div>
-    <div class="tinder--card">
-      <img src="https://placeimg.com/600/300/arch">
-      <h3>Demo card 5</h3>
-      <p>This is a demo for Tinder like swipe cards</p>
-    </div> -->
-=======
     
  
->>>>>>> fe2468f834d5dec075433fdb983f8f3da4e851d0
   </div>
 
     <div class="">
