05b53ad9f35ad287d89098693b401d63b21f2d67
Nora Marcelli <noramarcelli@gmail.com>
2018-05-03 17:56:25 +0300

    all input designs

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,10 +1,12 @@
 <template>
      <section class="register-page">
-        <h1>Register:</h1>
+        <h1>Please Register:</h1>
         <form @submit.prevent="register">
-            <input type="text" placeholder="username" v-model="user.name" />
-            <input type="password" placeholder="password" v-model="user.password" />
-            <button type="submit">Register</button>
+            <input class="input is-danger is-small is-rounded" type="text" placeholder="username" v-model="user.name" />
+            <br>
+            <input class="input is-danger is-small is-rounded" type="password" placeholder="password" v-model="user.password" />
+             <br>
+            <button class="button is-danger is-rounded" type="submit">Register</button>
         </form>
     </section>
 </template>
