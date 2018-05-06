c57ea650261fc8cb69957cf77b68257e259dfa6a
Nora Marcelli <noramarcelli@gmail.com>
2018-05-06 16:25:13 +0300

    carousel classes

--- src/components/DogCarousel.vue
+++ src/components/DogCarousel.vue
@@ -1,11 +1,15 @@
 <template>
-<!-- <div > -->
-    <carousel class="carousel" v-if="dog" :perPageCustom="[[480, 1], [768, 1]]">
-        <slide v-for="(img, idx) in dog.imgs" :key="idx">
-            <img :src="'./' + dog.imgs[idx]"/>
-        </slide>
-    </carousel>
-<!-- </div> -->
+<div class="carousel carousel-animated carousel-animate-slide">
+    <div class='carousel-container'>
+        <div class='carousel-item is-active'>
+            <carousel v-if="dog" :perPageCustom="[[480, 1], [768, 1]]">
+                <slide class='carousel-item' v-for="(img, idx) in dog.imgs" :key="idx">
+                    <img :src="'./' + dog.imgs[idx]"/>
+                </slide>
+            </carousel>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
@@ -20,8 +24,9 @@ export default {
 </script>
 
 <style scoped>
-carousel {
+carousel, .VueCarousel-pagination {
     padding: 0px;
     margin: 0px;
 }
+
 </style>
