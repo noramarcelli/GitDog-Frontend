17f252c22ee2d810b66cc9e2ed6c32f17c5e12f9
Nora Marcelli <noramarcelli@gmail.com>
2018-05-07 16:51:13 +0300

    edit input

--- src/views/EditPage.vue
+++ src/views/EditPage.vue
@@ -3,10 +3,9 @@
         <a @click="$router.push('/profile')" title="Done"><i class="fa fa-check-circle"></i></a>
         <p>This is a Temp Edit Page</p>
         
-        <span class="profile-imgs">
+        <section class="profile-imgs">
             <div class="block">
                 <div class="columns">
-
                     <div class="column is-8">
                         <!-- PROFILE PICTURE - THE FIRST -->
                         <img src="../../public/img/dogs/stella_1.jpeg">
@@ -20,19 +19,36 @@
                     </div>
                     
                 </div>
-            <div class="columns">
-                <div class="column is-4">
-                        <img src="../../public/img/dogs/stella_1.jpeg">
-                    </div>
+                <div class="columns">
                     <div class="column is-4">
-                        <img src="../../public/img/dogs/stella_1.jpeg">
-                    </div>
-                    <div class="column is-4">
-                        <img src="../../public/img/dogs/stella_1.jpeg">
-                    </div>
+                            <img src="../../public/img/dogs/stella_1.jpeg">
+                        </div>
+                        <div class="column is-4">
+                            <img src="../../public/img/dogs/stella_1.jpeg">
+                        </div>
+                        <div class="column is-4">
+                            <img src="../../public/img/dogs/stella_1.jpeg">
+                        </div>
+                </div>
             </div>
-            </div>
-        </span>
+        </section>
+
+    <section class="edit-details">
+        <!-- <input class="message-header" type="text" v-model="setPlaceToEdit.name"/> -->
+        <!-- RENDER NAME OF THE DOG -->
+        <input class="input" type="text"/>
+        <br>
+        
+        <div class="control">
+            <label class="radio is-small"><input type="radio" name="answer" checked>Female</label>
+            <label class="radio is-small"><input type="radio" name="answer">Male</label>
+        </div>
+
+
+        <!-- <textarea class="textarea" maxlength="40" size="60" type="text" v-model="setPlaceToEdit.desc"></textarea> -->
+        <!-- RENDER DESC OF THE DOG -->
+        <textarea class="textarea" maxlength="40" size="60" type="text"></textarea>
+    </section>
 
     </section>
 </template>
@@ -41,3 +57,8 @@
 <script>
 export default {};
 </script>
+
+<style>
+
+</style>
+
