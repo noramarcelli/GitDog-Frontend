a490694748185bfafc2cf7bf8a4354a9c64292cd
Nora Marcelli <noramarcelli@gmail.com>
2018-05-07 14:36:16 +0300

    merged

--- src/views/EditPage.vue
+++ src/views/EditPage.vue
@@ -1,6 +1,9 @@
 <template>
     <section class="edit-page">
-    <p>This is a Temp Edit Page</p>
+        <p>This is a Temp Edit Page</p>
+        <!-- <form @submit.prevent="checkLogin">
+            <input type="search" class="input is-danger is-small is-rounded" ref="txtUserName" type="text" placeholder="User name" v-model="user.name" autofocus/>
+        </form> -->
     </section>
 </template>
 
