077cd3d40a61bfaeb8e49b7e3b6661dc0c55a9ce
Nora Marcelli <noramarcelli@gmail.com>
2018-05-07 15:55:34 +0300

    edit profile pictures

--- src/views/EditPage.vue
+++ src/views/EditPage.vue
@@ -13,24 +13,26 @@
                     </div>
                     <div class="column is-4">
                         <img src="../../public/img/dogs/stella_1.jpeg">
-                        <a><i class="fa fa-plus-circle"></i></a>
+                        <!-- <a><i class="fa fa-plus-circle"></i></a> -->
                         <img src="../../public/img/dogs/stella_1.jpeg">
                         <!-- <a><i class="fa fa-plus-circle"></i></a> -->
                     </div>
-                    <div class="column is-4">
-                        <img src="../../public/img/dogs/stella_1.jpeg">
-                    </div>
-                    <div class="column is-4">
-                        <img src="../../public/img/dogs/stella_1.jpeg">
-                    </div>
-                    <div class="column is-4">
-                        <img src="../../public/img/dogs/stella_1.jpeg">
-                    </div>
-
+                    
                 </div>
+            <div class="columns">
+                <div class="column is-4">
+                        <img src="../../public/img/dogs/stella_1.jpeg">
+                    </div>
+                    <div class="column is-4">
+                        <img src="../../public/img/dogs/stella_1.jpeg">
+                    </div>
+                    <div class="column is-4">
+                        <img src="../../public/img/dogs/stella_1.jpeg">
+                    </div>
+            </div>
             </div>
         </span>
-        
+
     </section>
 </template>
 
