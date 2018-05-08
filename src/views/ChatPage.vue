0cea61c86f2f6e185eb57c7766d26e23d90f27ad
Nora Marcelli <noramarcelli@gmail.com>
2018-05-08 16:57:39 +0300

    chat window

---
+++ src/views/ChatPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="chat-page">
+    <a @click="$router.push('/chat')" title="Done"><i class="fa fa-check-circle"></i></a>
+    <p>This is a Temp chat Page</p>
+    <a @click="$router.push('/chat')" title="Send"><i class="fa fa-location-arrow"></i></a>
+    </section>
+</template>
+
+
+<script>
+export default {
+  
+}
+</script>
