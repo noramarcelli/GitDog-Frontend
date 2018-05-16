e061055d588ade0141e4a4a682615f86c56bbe0f
Ilana Mostovski <ilana.mostovski@gmail.com>
2018-05-16 19:02:06 +0300

    comments removed from edit page

--- src/store/dogStore.js
+++ src/store/dogStore.js
@@ -10,19 +10,11 @@ export default {
     selectedDogIdx: 0,
     filterBy: {},
     dogs: []
-    // times: 0,
-    // fromDog: null
   },
   getters: {
     currentDog: ({ dogs, selectedDogIdx }) => dogs[selectedDogIdx],
-    // fromDog(state){
-    //   return state.fromDog 
-    // }
   },
   mutations: {
-    // incTimes(state){
-    //   state.times++
-    // },
     setDogs(state, { dogs }) {
       state.selectedDogIdx = 0;
       state.dogs = dogs;
@@ -47,38 +39,6 @@ export default {
     }
   },
   actions: {
-    // moveCurrentDog({state, getters, commit}) {
-    //   if(state.dogs.length === 0) {
-    //     return DogService.getNextDogs( undefined, getters.userDogId)
-    //     .then(dogs => {
-    //       // if(this.state.filterBy && this.state.filterBy.cities && this.state.filterBy.weightGroups){
-
-    //       // }
-
-    //       // if (joinedMatch.dog.name.toLowerCase().includes(store.state.filterBy.name.toLowerCase())) {
-    //       //   filteredMatches.push(joinedMatch);
-    //       // }
-    //       commit({type:'setDogs',dogs})
-    //     });      
-    //   }
-    //   if (state.selectedDogIdx === state.dogs.length - 1 ) {
-    //     return DogService.getNextDogs(getters.currentDog._id, getters.userDogId)
-    //     .then(dogs => {
-    //       // if (dogs.length === 0) console.log(dogs)
-    //       // if(dogs === 'no dogs') console.log(dogs)
-    //       if(dogs === 'no dogs') {
-    //         return DogService.getNextDogs( undefined, getters.userDogId)
-    //         .then(dogs => {
-    //           commit({type:'setDogs',dogs})
-    //         });   
-    //       }
-    //       else commit({type:'setDogs',dogs})
-    //     });
-    //   } 
-    //   commit('moveCurrentDog')
-    //   return Promise.resolve()
-    // },
-
     moveCurrentDog({state, getters, commit}) {
       if(state.dogs.length === 0) {
         return DogService.getNextDogs( undefined, getters.userDogId)
@@ -89,8 +49,6 @@ export default {
       if (state.selectedDogIdx === state.dogs.length - 1 ) {
         return DogService.getNextDogs(getters.currentDog._id, getters.userDogId)
         .then(dogs => {
-          // if (dogs.length === 0) console.log(dogs)
-          // if(dogs === 'no dogs') console.log(dogs)
           if(dogs === 'no dogs') {
             return DogService.getNextDogs( undefined, getters.userDogId)
             .then(dogs => {
@@ -126,18 +84,12 @@ export default {
     },
 
     uploadImg(store, { imgUrl }) {
-      // console.log('userDog', dog);
-      // console.log("imgUrl in uploadImage in dogStore", imgUrl);
-
       return DogService.uploadImg(imgUrl).then(res => {
-        // console.log("res in uploadImg in dogStore", res);
-        // dog.imgs[idx] = imgUrl;
         return res;
       });
     },
 
     saveDog(store, { dogToEdit }) {
-      // console.log("dog inside dogStore", dogToEdit);
       return DogService.saveDog(dogToEdit).then(dogToEdit => {
         return dogToEdit;
       });
